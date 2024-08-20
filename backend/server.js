a1e5f6f2d2e3399e9a835f391a95f749a16e3e10
gautamkandpal <gkandpal33@gmail.com>
2024-08-20 23:26:30 +0530

    feat:added schema needed with the mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
+connectDB(); //Connect to MongoDB
+
 const app = express();
-const port = process.env.PORT || 5000; //added fallback port
+//add fallback port --> || 5000
+const port = process.env.PORT;
 
 app.get("/api/products", (req, res) => {
   res.json(products);
